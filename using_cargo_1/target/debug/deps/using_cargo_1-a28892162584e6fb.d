07eea688d1765f6690306c9e68e93846b3c47e7b
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-08 05:31:57 +0700

    chore: directory name to be compatible with cargo

---
+++ using_cargo_1/target/debug/deps/using_cargo_1-a28892162584e6fb.d
@@ -0,0 +1,5 @@
+/home/pendaurulang/Desktop/araggna/Study/rust/using_cargo_1/target/debug/deps/using_cargo_1-a28892162584e6fb: src/main.rs
+
+/home/pendaurulang/Desktop/araggna/Study/rust/using_cargo_1/target/debug/deps/using_cargo_1-a28892162584e6fb.d: src/main.rs
+
+src/main.rs:
---
+++ using_cargo_1/target/debug/using_cargo_1.d
@@ -0,0 +1,1 @@
+/home/pendaurulang/Desktop/araggna/Study/rust/using_cargo_1/target/debug/using_cargo_1: /home/pendaurulang/Desktop/araggna/Study/rust/using_cargo_1/src/main.rs
