8d52148e22564015bf7cf01d830ffa767a6a6ad3
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-10 04:22:58 +0700

    chore: simple shadowing

--- variables_3/src/main.rs
+++ variables_3/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
+    //muttability
     let mut x = 5;
     println!("x value is {}", x);
     x = 6;
     println!("current x value is {}", x);
+
+    //shadowing
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("current y value is {}", y);
 }
