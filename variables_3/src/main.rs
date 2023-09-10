d33f7b5f393bc0a4e2633b4e2128a90ebf20fbe2
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-11 05:52:13 +0700

    chore: constant, int type, floating pont type, numeric operation

--- variables_3/src/main.rs
+++ variables_3/src/main.rs
@@ -10,4 +10,30 @@ fn main() {
     let y = y + 1;
     let y = y * 2;
     println!("current y value is {}", y);
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("lenght of space is {}", spaces);
+
+    // constant
+    const MAX_POINTS: u32 = 100_000;
+    println!("current MAX_POINT value is {}", MAX_POINTS);
+
+    // int type
+    let _decimal = 98_000;
+    let _hex = 0xffffff;
+    let _octal = 0o66;
+    let _binary: i32 = 0b1110011;
+    let _byte: u8 = b'X'; //u8 only
+
+    // floating point type
+    let _fp_default = 10.2;
+    let _fp_f32: f32 = 1.2;
+
+    // numeric operation
+    let _addition = 5 + 9;
+    let _substraction = 10 / 2;
+    let _multiplication = 6 * 6;
+    let _division = 20 / 4;
+    let _remainder = 21 % 4;
 }
