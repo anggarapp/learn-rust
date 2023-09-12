c4a8676aeb5e9ab7de21097796e48bbc7bb91a9c
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-13 05:47:41 +0700

    chore: function parameters

--- function_4/src/main.rs
+++ function_4/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
     println!("Hello, Kid!");
-    another_function(15);
+    another_function(15, 2);
 }
 
-fn another_function(x: i32) {
-    println!("another_function parameter is : {}", x);
+fn another_function(x: i32, y: i32) {
+    println!("first another_function parameter is : {}", x);
+    println!("second another_function parameter is : {}", y);
 }
