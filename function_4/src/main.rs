7fa23b80d88fa3b801a59516db3ea0a4806a4a5c
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-13 05:55:11 +0700

    chore: statement, function with return value

--- function_4/src/main.rs
+++ function_4/src/main.rs
@@ -1,9 +1,29 @@
 fn main() {
     println!("Hello, Kid!");
+
+    // function
     another_function(15, 2);
+
+    // statement
+    let first_statement = 3;
+    let second_statement = {
+        let x = 52;
+        x / 2
+    };
+    println!(
+        "The first statement is: {}, and second is: {}",
+        first_statement, second_statement
+    );
+
+    let six_statement = six();
+    println!("its already: {}", six_statement);
 }
 
 fn another_function(x: i32, y: i32) {
     println!("first another_function parameter is : {}", x);
     println!("second another_function parameter is : {}", y);
+}
+
+fn six() -> i32 {
+    6
 }
