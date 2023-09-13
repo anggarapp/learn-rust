346f976e5fd0766bbc06abcf0703ce4025ac7110
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-13 19:04:45 +0700

    add: branches part

---
+++ branches_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    _ifelse();
+}
+
+fn _ifelse() {
+    let number = 40;
+    if number < 5 && number != 0 {
+        println!("this is true value");
+    } else {
+        println!("this is false value");
+    }
+}
