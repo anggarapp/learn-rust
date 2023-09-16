b046e54f79b6b0c831e9a5c06abc15d9fed20f95
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-16 14:36:47 +0700

    chore: default copy behavior of number data

--- init_ownership_6/src/main.rs
+++ init_ownership_6/src/main.rs
@@ -1,5 +1,33 @@
 fn main() {
+    // _using_clone();
+    _default_copy_of_number_data();
+}
+
+fn _using_clone() {
     let mut s = String::from("Aye");
     s.push_str(", Kiddos!");
-    println!("{}", s);
+
+    let t = s.clone();
+    println!("s = {} must same with t = {}", s, t);
+}
+
+fn _default_copy_of_number_data() {
+    let int: i32 = 2;
+    let re_int: i32 = int;
+
+    assert_eq!(int, re_int);
+
+    let float: f64 = 2.2;
+    let re_float: f64 = float;
+
+    assert_eq!(float, re_float);
+
+    let tuple_of_number: (i32, i32) = (2, 3);
+    let re_tuple_of_number: (i32, i32) = tuple_of_number;
+
+    assert_eq!(tuple_of_number, re_tuple_of_number);
+
+    //it will error
+    // let tuple_of_mix: (i32, String) = (2, String::from("Error"));
+    // let re_tuple_of_mix: (i32, String) = tuple_of_mix;
 }
