3a0b5d7c5985145444cc7efdc915354f50cebb5f
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-19 23:37:19 +0700

    chore: muttable references

--- references_borrowing_8/src/main.rs
+++ references_borrowing_8/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-    let s1 = String::from("ello cruel world");
+    let mut s1 = String::from("welcome to cruel world");
     let len = calculate_length_with_ref(&s1);
     println!("The length of {} is : {}", s1, len);
+    change(&mut s1)
 }
 
 fn calculate_length_with_ref(s: &String) -> usize {
     s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", Kiddo");
 }
