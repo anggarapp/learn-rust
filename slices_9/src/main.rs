d645bda66cbd4bc68afbfdcc16ae7845921a06c0
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-20 05:06:48 +0700

    chore: using literal string

--- slices_9/src/main.rs
+++ slices_9/src/main.rs
@@ -1,22 +1,26 @@
 fn main() {
     let mut the_string: String = String::from("Some Strings");
 
-    let slice = &the_string[0..4]; //
-    let slice = &the_string[..4]; // same
+    // let _slice = &the_string[0..4]; //
+    // let _slice = &the_string[..4]; // same
 
-    let len = the_string.len();
+    // let len = the_string.len();
 
-    let slices = &the_string[6..len]; //
-    let slices = &the_string[6..]; // same
+    // let _slices = &the_string[6..len]; //
+    // let _slices = &the_string[6..]; // same
 
-    let all_slices = &the_string[..]; //take all slices
+    // let _all_slices = &the_string[..]; //take all slices
 
-    let _word = first_word(&the_string);
-    println!("the first word is {}", _word);
-    the_string.clear();
+    let word = first_word(&the_string);
+    println!("the first word is {}", word);
+
+    let literal = "Literal String";
+    let literal_first_word = first_word(&literal[..]);
+
+    println!("the first word is {}", literal_first_word);
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
