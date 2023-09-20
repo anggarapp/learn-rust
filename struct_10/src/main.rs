7df1a30cfcadbaa8d0ec26e25a3dc16bec129d82
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-21 05:45:24 +0700

    chore: tuple struct

--- struct_10/src/main.rs
+++ struct_10/src/main.rs
@@ -1,9 +1,13 @@
+// Basic Struct
 struct User {
     username: String,
     email: String,
     sign_in_count: u64,
     active: bool,
 }
+
+// Tuple Struct
+struct RGB(i32, i32, i32);
 
 fn main() {
     let _user_1 = User {
@@ -12,7 +16,22 @@ fn main() {
         active: true,
         sign_in_count: 1,
     };
+    // create struct using function
     let _user_2 = user_builder(String::from("test"), String::from("email"));
+
+    // create struct from other struct value
+    let _user_3 = User {
+        email: String::from("email@mail.com"),
+        username: String::from("laplace"),
+        active: _user_1.active,
+        sign_in_count: _user_1.sign_in_count,
+    };
+    // create struct, with else fields same with other struct fields
+    let _user_4 = User {
+        email: String::from("email@mail.com"),
+        username: String::from("laplace"),
+        .._user_3
+    };
 }
 
 fn user_builder(user: String, email: String) -> User {
