31f95ad2f8b6e86ce69fde244b8ade7293e976a0
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-21 05:31:04 +0700

    chore: struct object builder

--- struct_10/src/main.rs
+++ struct_10/src/main.rs
@@ -6,10 +6,20 @@ struct User {
 }
 
 fn main() {
-    let user_1 = User {
+    let _user_1 = User {
         email: String::from("email@mail.com"),
         username: String::from("laplace"),
         active: true,
         sign_in_count: 1,
     };
+    let _user_2 = user_builder(String::from("test"), String::from("email"));
+}
+
+fn user_builder(user: String, email: String) -> User {
+    User {
+        username: user,
+        email: email,
+        sign_in_count: 1,
+        active: true,
+    }
 }
