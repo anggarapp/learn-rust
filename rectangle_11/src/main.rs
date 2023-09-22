04afb91e56670a4b711b66024b0a71582447606e
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-22 21:11:25 +0700

    chore: practical use of tuple and struct

---
+++ rectangle_11/src/main.rs
@@ -0,0 +1,33 @@
+struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+fn main() {
+    let _width_1 = 30;
+    let _length_1 = 40;
+    let _rec_tuple = (10, 23);
+    let _rec_struct = Rectangle {
+        length: 60,
+        width: 11,
+    };
+
+    println!(
+        "Area based two of side is {}",
+        area_ordinary(_length_1, _width_1)
+    );
+    println!("Area based on tuple is {}", area_tuple(_rec_tuple));
+    println!("Area based on struct is {}", area_struct(&_rec_struct));
+}
+
+fn area_ordinary(length: u32, width: u32) -> u32 {
+    length * width
+}
+
+fn area_tuple(dimension: (u32, u32)) -> u32 {
+    dimension.0 * dimension.1
+}
+
+fn area_struct(rectangle: &Rectangle) -> u32 {
+    rectangle.length * rectangle.width
+}
