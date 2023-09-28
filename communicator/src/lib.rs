ee6b334fb16ca6fb80b2163aad81489a80808d2a
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-29 05:22:26 +0700

    chore: init module

---
+++ communicator/src/lib.rs
@@ -0,0 +1,21 @@
+mod network {
+    fn connect() {}
+}
+mod client {
+    fn connect() {}
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
--- enums_12/src/main.rs
+++ enums_12/src/main.rs
@@ -45,7 +45,6 @@ fn main() {
 
     let some_u8 = Some(0u8);
 
-    
     match some_u8 {
         Some(3) => println!("Tiga"),
         _ => (),
