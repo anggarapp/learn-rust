0c3c3683396e000fa9eb959f1be074a29d92a6e4
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-30 05:55:20 +0700

    chore: module privacy fix

--- privacy_rules_14/src/lib.rs
+++ privacy_rules_14/src/lib.rs
@@ -1,17 +1,18 @@
 mod outermost {
     pub fn middle_function() {}
 
-    fn middle_secret_function() {}
+    pub fn middle_secret_function() {} //fix
 
-    mod inside {
+    pub mod inside {
+        //fix
         pub fn inner_function() {}
-        fn inner_secret_function() {}
+        pub fn inner_secret_function() {} //fix
     }
 }
 
 fn try_me() {
     outermost::middle_function();
-    // outermost::middle_secret_function();  // not work because function is private
-    // outermost::inside::inner_function(); // not work because module is private
-    // outermost::inside::inner_secret_function(); // not work because module & function is private
+    outermost::middle_secret_function(); // not work because function is private
+    outermost::inside::inner_function(); // not work because module is private
+    outermost::inside::inner_secret_function(); // not work because module & function is private
 }
