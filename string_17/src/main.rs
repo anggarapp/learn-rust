52bd5500ab97e79802b92af26215c913a7cda50c
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-09-30 18:47:31 +0700

    chore: string init

---
+++ string_17/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut _init_string = String::new();
+    let _data: &str = "initial contents";
+    let _s = _data.to_string();
+    let _s = "initial contents".to_string();
+    let _s = String::from("initial contents");
+    println!("{}", _s);
+}
