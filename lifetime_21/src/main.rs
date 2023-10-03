7fc492092f4059cf3e93b659faa9d532aa566666
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-04 05:16:44 +0700

    chore: lifetime specifier

--- lifetime_21/src/main.rs
+++ lifetime_21/src/main.rs
@@ -6,4 +6,18 @@ fn main() {
         println!("r: {}", r);
     }
     // println!("r: {}", r); //the lifetime not long enough to borrowing here
+
+    let string_1 = String::from("despair");
+    let string_2 = "emptiness";
+
+    let result = longest(string_1.as_str(), string_2);
+    println!("The longest string is {}", result);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
