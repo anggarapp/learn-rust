0e52467da0b0a60d2b51e65bda548b0850df1dc2
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-03 18:33:30 +0700

    chore: using generic on struct

--- generic_20/src/main.rs
+++ generic_20/src/main.rs
@@ -1,4 +1,9 @@
-fn largest(list: &[i32]) -> i32 {
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn _largest(list: &[i32]) -> i32 {
     let mut largest = list[0];
     for &item in list.iter() {
         if item > largest {
@@ -10,7 +15,10 @@ fn largest(list: &[i32]) -> i32 {
 
 fn main() {
     let _numbers = vec![34, 55, 22, 101, 66];
-    let mut _largest = largest(&_numbers);
+    let mut _largest = _largest(&_numbers);
     println!("largest number is {}", _largest);
     assert_eq!(_largest, 101);
+
+    let _int = Point { x: 5, y: 12 };
+    let _flt = Point { x: 6.6, y: 12.9 };
 }
