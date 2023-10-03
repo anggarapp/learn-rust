3b1e2f1f0b277d742592b2d2396bbf0121dce4b1
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-03 18:08:35 +0700

    chore: using function - find largest number

--- generic_20/src/main.rs
+++ generic_20/src/main.rs
@@ -1,13 +1,16 @@
-fn main() {
-    let _numbers = vec![34, 55, 22, 101, 66];
-    let mut _largest = _numbers[0];
-
-    for number in _numbers {
-        if number > _largest {
-            _largest = number;
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
         }
     }
+    largest
+}
 
+fn main() {
+    let _numbers = vec![34, 55, 22, 101, 66];
+    let mut _largest = largest(&_numbers);
     println!("largest number is {}", _largest);
     assert_eq!(_largest, 101);
 }
