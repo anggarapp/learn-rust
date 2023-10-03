51c28ac9b318e7d2d0d99401f9c951e6c54c12c8
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-03 16:30:21 +0700

    chore: error on unwrap and expect

--- panic_19/src/main.rs
+++ panic_19/src/main.rs
@@ -2,22 +2,27 @@ use std::fs::File;
 use std::io::ErrorKind;
 fn main() {
     // panic!("farewell");
+    // using unwrap
+    // let _file = File::open("test.txt").unwrap();
 
-    let _file = File::open("test.txt");
+    // using expect
+    let _file = File::open("test.txt").expect("Failed to Open test.txt");
 
-    let _file = match _file {
-        Ok(file) => file,
-        Err(ref error) if error.kind() == ErrorKind::NotFound => {
-            // panic!("Problem here Kiddos! {:?}", error)
-            match File::create("test.txt") {
-                Ok(fclose) => fclose,
-                Err(err) => {
-                    panic!("Tried to create file but there was problem: {:?}", err)
-                }
-            }
-        }
-        Err(error) => {
-            panic!("there was problem opening file: {:?}", error);
-        }
-    };
+    // let _file = File::open("test.txt");
+
+    // let _file = match _file {
+    //     Ok(file) => file,
+    //     Err(ref error) if error.kind() == ErrorKind::NotFound => {
+    //         // panic!("Problem here Kiddos! {:?}", error)
+    //         match File::create("test.txt") {
+    //             Ok(fclose) => fclose,
+    //             Err(err) => {
+    //                 panic!("Tried to create file but there was problem: {:?}", err)
+    //             }
+    //         }
+    //     }
+    //     Err(error) => {
+    //         panic!("there was problem opening file: {:?}", error);
+    //     }
+    // };
 }
