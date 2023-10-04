311396b0f0da746c4b082cf1e12e90e40597dc34
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-05 02:27:59 +0700

    chore: using assert

--- adder_22/src/lib.rs
+++ adder_22/src/lib.rs
@@ -2,6 +2,18 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+#[derive(Debug)]
+pub struct Rectangle {
+    length: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    pub fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -12,8 +24,33 @@ mod tests {
         assert_eq!(result, 4);
     }
 
+    // #[test]
+    // fn panicking() {
+    //     panic!("Make test fail");
+    // }
+
     #[test]
-    fn panicking() {
-        panic!("Make test fail");
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            length: 32,
+            width: 32,
+        };
+        let smaller = Rectangle {
+            length: 12,
+            width: 12,
+        };
+        assert!(larger.can_hold(&smaller));
+    }
+    #[test]
+    fn smaller_cant_hold_larger() {
+        let larger = Rectangle {
+            length: 32,
+            width: 32,
+        };
+        let smaller = Rectangle {
+            length: 12,
+            width: 12,
+        };
+        assert!(!smaller.can_hold(&larger));
     }
 }
