c74a8d26a9c9b6aad71e7e00fd2f8cd170ece08f
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-05 02:12:25 +0700

    chore: lifetime with generic type param

--- lifetime_21/src/main.rs
+++ lifetime_21/src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 struct ImportantExcerpt<'a> {
     part: &'a str,
 }
@@ -14,6 +16,17 @@ impl<'a> ImportantExcerpt<'a> {
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+fn longest_with_annoucement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
     if x.len() > y.len() {
         x
     } else {
