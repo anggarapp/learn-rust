fa58c8d5ceb1f8060f9c83c3353cfbe63139c0c8
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-05 03:11:30 +0700

    chore: using should_panic and with specific panic messages

--- adder_22/src/lib.rs
+++ adder_22/src/lib.rs
@@ -23,6 +23,29 @@ impl Rectangle {
     }
 }
 
+struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be less than or equal to 100,
+            got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be greater than or equal to
+            1, got {}.",
+                value
+            );
+        }
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -68,13 +91,25 @@ mod tests {
         };
         assert!(!smaller.can_hold(&larger));
     }
+    // #[test]
+    // fn greeting_contains_name() {
+    //     let result = greeting("Kafka");
+    //     assert!(
+    //         result.contains("Kafka"),
+    //         "Greeting di not contain name, value was `{}`",
+    //         result
+    //     );
+    // }
+
     #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Kafka");
-        assert!(
-            result.contains("Kafka"),
-            "Greeting di not contain name, value was `{}`",
-            result
-        );
+    #[should_panic]
+    fn greatee_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn smaller_than_1() {
+        Guess::new(0);
     }
 }
