d7abd8ce69a91dcf47d209730ed3fc8a2b865590
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-06 05:55:16 +0700

    chore: read text file

--- greprs_23/src/main.rs
+++ greprs_23/src/main.rs
@@ -1,6 +1,20 @@
 use std::env;
-
+use std::fs::File;
+use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let mut file = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("something went wrong on reading the file");
+
+    println!("With text:\n{}", contents);
 }
