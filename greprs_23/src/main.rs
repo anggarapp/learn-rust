0168f33f8ee06bc1b08b806d1d6b0c793f94da24
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-06 08:03:38 +0700

    chore: refactor config error handling

--- greprs_23/src/main.rs
+++ greprs_23/src/main.rs
@@ -1,27 +1,38 @@
-use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::{env, process};
 
 struct Config {
     query: String,
     filename: String,
 }
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing argument: {}", err);
+        process::exit(1);
+    });
 
-    let mut file = File::open(filename).expect("file not found");
+    println!("Searching for {}", config.query);
+    println!("In File {}", config.filename);
+
+    let mut file = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     file.read_to_string(&mut contents)
         .expect("something went wrong on reading the file");
 
     println!("With text:\n{}", contents);
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config { query, filename }
 }
