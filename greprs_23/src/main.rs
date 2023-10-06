72e252c2df72d4c1e90a2c426ecf509e4a8d8449
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-07 04:56:00 +0700

    chore: using stderr on error output

--- greprs_23/src/main.rs
+++ greprs_23/src/main.rs
@@ -1,13 +1,17 @@
 extern crate greprs_23;
+use std::io::prelude::*;
 use std::{env, process};
 
 use greprs_23::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut stderr = std::io::stderr();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing argument: {}", err);
+        // println!("Problem parsing argument: {}", err);
+        writeln!(&mut stderr, "Problem parsing arguments: {}", err)
+            .expect("Could not write to stderr");
         process::exit(1);
     });
 
@@ -15,7 +19,8 @@ fn main() {
     println!("In File {}", config.filename);
 
     if let Err(e) = greprs_23::run(config) {
-        println!("Application error: {}", e);
+        // println!("Application error: {}", e);
+        writeln!(&mut stderr, "Application error: {}", e).expect("Could not write to stderr");
         process::exit(1);
     };
 }
