2235a2c22b4e2c353d6ad4ce9fb6f8b4356048db
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-07 00:18:03 +0700

    chore: add first test case

--- greprs_23/src/lib.rs
+++ greprs_23/src/lib.rs
@@ -27,3 +27,28 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("With text:\n{}", contents);
     Ok(())
 }
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+save, fast, productive.
+Pick three.";
+        assert_eq!(vec!["save, fast, productive."], search(query, contents));
+    }
+}
