a5e2cd1b50722ebe82388409009e69f590841a53
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-07 18:07:39 +0700

    chore: using closure with Generic parameter and Fn Traits

--- closures_24/src/main.rs
+++ closures_24/src/main.rs
@@ -1,32 +1,66 @@
 use std::thread;
 use std::time::Duration;
 
+struct Cacher<T>
+where
+    T: Fn(i32) -> i32,
+{
+    calculation: T,
+    value: Option<i32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(i32) -> i32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, args: i32) -> i32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(args);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
 fn main() {
     let simulatd_user_specified_value = 10;
     let simulated_random_number = 7;
 
     generate_workout(simulatd_user_specified_value, simulated_random_number);
 }
-fn simulated_expensive_calculation(intensity: i32) -> i32 {
+fn _simulated_expensive_calculation(intensity: i32) -> i32 {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
     intensity
 }
 
 fn generate_workout(intensity: i32, random_number: i32) {
-    let expensive_result = |num| {
+    let mut expensive_result = Cacher::new(|num: i32| -> i32 {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_result(intensity));
-        println!("Next, do {} situps!", expensive_result(intensity));
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated");
         } else {
-            println!("Today, run for {} minutes!", expensive_result(intensity))
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            )
         }
     }
 }
