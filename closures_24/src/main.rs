1d9e3071e3c970a964b41046d6ade1ddab9f1eef
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-07 17:43:49 +0700

    chore: change simulated_expense_calculation function to closure

--- closures_24/src/main.rs
+++ closures_24/src/main.rs
@@ -14,23 +14,19 @@ fn simulated_expensive_calculation(intensity: i32) -> i32 {
 }
 
 fn generate_workout(intensity: i32, random_number: i32) {
+    let expensive_result = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
     if intensity < 25 {
-        println!(
-            "Today, do {} pushups!",
-            simulated_expensive_calculation(intensity)
-        );
-        println!(
-            "Next, do {} situps!",
-            simulated_expensive_calculation(intensity)
-        );
+        println!("Today, do {} pushups!", expensive_result(intensity));
+        println!("Next, do {} situps!", expensive_result(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated");
         } else {
-            println!(
-                "Today, run for {} minutes!",
-                simulated_expensive_calculation(intensity)
-            )
+            println!("Today, run for {} minutes!", expensive_result(intensity))
         }
     }
 }
