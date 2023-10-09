ac686ad15f6ea1ba1a0c21f7c3abaec5d01dd8ef
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-10 05:59:00 +0700

    chore: implement iterator on struct

--- closures_24/src/lib.rs
+++ closures_24/src/lib.rs
@@ -4,6 +4,29 @@ struct Shoe {
     style: String,
 }
 
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: i32) -> Vec<Shoe> {
     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }
