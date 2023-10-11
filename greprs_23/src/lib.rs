137b18e4f5ed598371bff64859e45d6d2ca03c05
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-11 14:15:25 +0700

    chore: tidy up freprs search using iterator

--- greprs_23/src/lib.rs
+++ greprs_23/src/lib.rs
@@ -46,6 +46,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+fn _before_search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
     for line in contents.lines() {
         if line.contains(query) {
