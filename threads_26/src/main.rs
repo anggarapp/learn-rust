9bddb0aeab843701ddaf49697d5f77fca5316aa7
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-16 19:43:57 +0700

    chore: add move property on thread closures

--- threads_26/src/main.rs
+++ threads_26/src/main.rs
@@ -1,7 +1,8 @@
 use std::thread;
 
 fn main() {
-    _demo_init_threads();
+    // _demo_init_threads();
+    _demo_move_on_closure();
 }
 
 fn _demo_init_threads() {
@@ -15,5 +16,13 @@ fn _demo_init_threads() {
         println!("this is number {} from main threads kiddos", i);
     }
 
+    handle.join();
+}
+
+fn _demo_move_on_closure() {
+    let v = vec![1, 2, 3];
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
     handle.join();
 }
