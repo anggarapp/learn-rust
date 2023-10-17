59401e42be0a44217553dc396864ede151f7035f
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-18 05:48:20 +0700

    chore: ignoring remaining values with ..

--- pattern_matching_28/src/main.rs
+++ pattern_matching_28/src/main.rs
@@ -13,7 +13,30 @@ fn main() {
     // _destruct_enum();
     // _destruct_nested_enum_struct();
     // _destruct_struct_tuples();
-    _ignoring_nested();
+    // _ignoring_nested();
+    _ignoring_remaining();
+}
+
+fn _ignoring_remaining() {
+    struct Point {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+
+    let origin = Point { x: 0, y: 0, z: 0 };
+
+    match origin {
+        Point { x, .. } => println!("x is {}", x),
+    }
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {first}, {last}");
+        }
+    }
 }
 
 fn _ignoring_nested() {
