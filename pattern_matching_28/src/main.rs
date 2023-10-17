0124d9b79b11c08eb37f32d80a2b217e262b2689
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-18 05:23:28 +0700

    chore: destruct struct with literal matching

--- pattern_matching_28/src/main.rs
+++ pattern_matching_28/src/main.rs
@@ -8,7 +8,29 @@ fn main() {
     // _matching_literals();
     // _named_variables();
     // _multiple_pattern();
-    _values_range();
+    // _values_range();
+    _destruct_struct();
+}
+
+fn _destruct_struct() {
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let p = Point { x: 0, y: 7 };
+
+    // let Point { x: a, y: b } = p;
+    // assert_eq!(0, a);
+    // assert_eq!(7, b);
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
 }
 
 fn _values_range() {
