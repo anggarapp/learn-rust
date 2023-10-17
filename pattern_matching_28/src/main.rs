94855675a156f621c7101d44d942d95e6ca41bad
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-17 14:48:31 +0700

    chore: function parameter

--- pattern_matching_28/src/main.rs
+++ pattern_matching_28/src/main.rs
@@ -4,6 +4,14 @@ fn main() {
     // _while_let();
     // _for_loops();
     // _let_deconstruct();
+    _function_parameter();
+}
+fn _function_parameter() {
+    fn print_coordinates(&(x, y): &(i32, i32)) {
+        println!("Current location: ({}, {})", x, y);
+    }
+    let point = (3, 5);
+    print_coordinates(&point);
 }
 
 fn _let_deconstruct() {
