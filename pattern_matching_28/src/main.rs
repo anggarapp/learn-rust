eed467d951fd2024ca90618cbcd45df289f31a04
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-18 05:40:55 +0700

    chore: destruct nested struct and enum

--- pattern_matching_28/src/main.rs
+++ pattern_matching_28/src/main.rs
@@ -10,7 +10,34 @@ fn main() {
     // _multiple_pattern();
     // _values_range();
     // _destruct_struct();
-    _destruct_enum();
+    // _destruct_enum();
+    _destruct_nested_enum_struct();
+}
+
+fn _destruct_nested_enum_struct() {
+    enum Color {
+        Rgb(i32, i32, i32),
+        Hsv(i32, i32, i32),
+    }
+
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(Color),
+    }
+
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change color to red {r}, green {g}, and blue {b}");
+        }
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!("Change color to hue {h}, saturation {s}, value {v}")
+        }
+        _ => (),
+    }
 }
 
 fn _destruct_enum() {
