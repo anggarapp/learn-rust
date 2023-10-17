6ec662921bd6c02b0504222fe34414a750611c51
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-17 12:00:35 +0700

    chore: implement state pattern

--- oop_27/src/main.rs
+++ oop_27/src/main.rs
@@ -1,34 +1,49 @@
-extern crate oop_27;
-use oop_27::{Button, Draw, Screen};
+// extern crate oop_27;
+// use oop_27::{Button, Draw, Screen};
 
-struct SelectBox {
-    width: u32,
-    height: u32,
-    options: Vec<String>,
-}
+// struct SelectBox {
+//     width: u32,
+//     height: u32,
+//     options: Vec<String>,
+// }
 
-impl Draw for SelectBox {
-    fn draw(&self) {}
-}
+// impl Draw for SelectBox {
+//     fn draw(&self) {}
+// }
+// fn main() {
+//     let screen = Screen {
+//         components: vec![
+//             Box::new(SelectBox {
+//                 width: 75,
+//                 height: 10,
+//                 options: vec![
+//                     String::from("Yes"),
+//                     String::from("Maybe"),
+//                     String::from("No"),
+//                 ],
+//             }),
+//             Box::new(Button {
+//                 width: 50,
+//                 height: 10,
+//                 label: String::from("OK"),
+//             }),
+//         ],
+//     };
+
+//     screen.run();
+// }
+
+use oop_27::Post;
+
 fn main() {
-    let screen = Screen {
-        components: vec![
-            Box::new(SelectBox {
-                width: 75,
-                height: 10,
-                options: vec![
-                    String::from("Yes"),
-                    String::from("Maybe"),
-                    String::from("No"),
-                ],
-            }),
-            Box::new(Button {
-                width: 50,
-                height: 10,
-                label: String::from("OK"),
-            }),
-        ],
-    };
+    let mut post = Post::new();
 
-    screen.run();
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
