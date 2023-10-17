61ddc47d46f60f85780c152c142a7ec6c021e227
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-17 14:38:43 +0700

    chore: using if let variant

--- pattern_matching_28/src/main.rs
+++ pattern_matching_28/src/main.rs
@@ -1,5 +1,27 @@
 fn main() {
-    _very_simple_match();
+    // _very_simple_match();
+    _if_let();
+}
+
+fn _if_let() {
+    // let favorite_color: Option<&str> = Some("Grey");
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as the background");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
 }
 
 fn _very_simple_match() {
