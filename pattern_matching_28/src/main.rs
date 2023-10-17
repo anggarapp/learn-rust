457ec06bed1211ec3a2d0dd1ecdeefbdf97eab70
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-18 05:46:27 +0700

    chore: ignoring value with nested _

--- pattern_matching_28/src/main.rs
+++ pattern_matching_28/src/main.rs
@@ -12,7 +12,32 @@ fn main() {
     // _destruct_struct();
     // _destruct_enum();
     // _destruct_nested_enum_struct();
-    _destruct_struct_tuples();
+    // _destruct_struct_tuples();
+    _ignoring_nested();
+}
+
+fn _ignoring_nested() {
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {first}, {third}, {fifth}")
+        }
+    }
 }
 
 fn _destruct_struct_tuples() {
