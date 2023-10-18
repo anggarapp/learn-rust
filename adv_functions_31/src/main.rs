829f31b8e58bc3613602af932e6e6983590954ee
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-18 18:55:54 +0700

    chore: function pointers

---
+++ adv_functions_31/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    _function_as_parameters();
+    _enum_as_initializer();
+}
+
+fn _function_as_parameters() {
+    fn add_one(x: i32) -> i32 {
+        x + 1
+    }
+
+    fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+        f(arg) + f(arg)
+    }
+
+    let answer = do_twice(add_one, 5);
+
+    println!("The answer is: {}", answer);
+}
+
+fn _enum_as_initializer() {
+    enum Status {
+        Value(u32),
+        Stop,
+    }
+
+    let list_of_statuses: Vec<Status> = (0u32..20).map(Status::Value).collect();
+}
