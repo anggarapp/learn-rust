77999767d6bc26f20fbfa9fcc8f5e5629432bc93
Anggara Permana Putra <anggaraperpe@gmail.com>
2023-10-18 13:48:48 +0700

    chore: calling same name method

--- adv_traits_29/src/main.rs
+++ adv_traits_29/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    _associated_type_on_trait();
+    // _associated_type_on_trait();
+    // _generic_type_parameters_operator_overloading();
+    _calling_same_name_method();
 }
 
 fn _associated_type_on_trait() {
@@ -57,4 +59,63 @@ fn _generic_type_parameters_operator_overloading() {
             Millimeters(self.0 + (other.0 * 1000))
         }
     }
+}
+
+fn _calling_same_name_method() {
+    trait Pilot {
+        fn fly(&self);
+    }
+
+    trait Wizard {
+        fn fly(&self);
+    }
+
+    struct Human;
+
+    impl Pilot for Human {
+        fn fly(&self) {
+            println!("This is your captain speaking.");
+        }
+    }
+
+    impl Wizard for Human {
+        fn fly(&self) {
+            println!("Up!");
+        }
+    }
+
+    impl Human {
+        fn fly(&self) {
+            println!("*waving arms furiously*");
+        }
+    }
+
+    let person = Human;
+    person.fly();
+
+    let person_1 = Human;
+    Pilot::fly(&person_1);
+    Wizard::fly(&person_1);
+    person_1.fly();
+
+    trait Animal {
+        fn baby_name() -> String;
+    }
+
+    struct Dog;
+
+    impl Dog {
+        fn baby_name() -> String {
+            String::from("Spot")
+        }
+    }
+
+    impl Animal for Dog {
+        fn baby_name() -> String {
+            String::from("puppy")
+        }
+    }
+
+    println!("A baby dog is called a {}", Dog::baby_name());
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
 }
